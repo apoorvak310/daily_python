10a9e8fffa79b0dc38acffbaa6108a1637fb99e6
Apoorva K <apoorvak310@gmail.com>
2026-09-09 13:23:37 +0530

    Add files via upload

---
+++ sept9.py
@@ -0,0 +1,56 @@
+# #1. 
+class Student:
+    def __init__(self,n,rno,m):
+        self.n = n
+        self.rno = rno
+        self.m = m
+    def display_info(self):
+        msg = f'Name: {self.n} Roll No.: {self.rno} Marks: {self.m}'
+        return msg
+s1 = Student('Rahul',101,85)
+res = s1.display_info()
+print(res)
+
+#2.
+class Rectangle:
+    def __init__(self,l,b):
+        self.l = l
+        self.b = b
+    def area(self):
+        return self.l * self.b
+    def perimeter(self):
+        return 2*(self.l + self.b)
+r1 = Rectangle(10,5)
+area = r1.area()
+perimeter = r1.perimeter()
+print(area)
+print(perimeter)
+
+#3.
+class Employee:
+    def __init__(self,name, e_id, sal):
+        self.name = name
+        self.e_id = e_id
+        self.sal = sal
+    def info(self):
+        display = f"Name: {self.name} Employee Id: {self.e_id} Salary: {self.sal}"
+        return display
+    def bonus(self, b):
+        new_sal = self.sal + b
+        return new_sal
+e1 = Employee('Arjun',101,30000)
+res = e1.bonus(1000)
+res2 = e1.info()
+print(res2)
+
+#4.
+class Mobile:
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+    def info(self):
+        return f"Brand: {self.brand} Model: {self.model} Price: {self.price}"
+m1 = Mobile('Samsung','A55',30000)
+res = m1.info()
+print(res)
